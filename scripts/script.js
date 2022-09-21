4026c48a41b87409daa7d97534cdb2e0fbe8410b
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2022-09-21 16:26:51 +0530

    all validations are done in JS itself instead of HTML as styling of error message/s will be easier

---
+++ scripts/script.js
@@ -0,0 +1,92 @@
+"use strict";
+
+
+//Business Logic in Plain English
+
+//User is shown form
+//In case User doesn't give valid input/s and submits form; s/he is shown proper error message/s
+
+
+//-----------------  DOM Elements  -----------------
+
+let firstName = document.getElementsByClassName("user-input__form-firstname")[0];
+let lastName = document.getElementsByClassName("user-input__form-lastname")[0];
+let email = document.getElementsByClassName("user-input__form-email")[0];
+let password = document.getElementsByClassName("user-input__form-password")[0];
+let submit = document.getElementsByClassName("user-input__form-button")[0];
+const form = document.getElementsByClassName("user-input__form")[0];
+
+const firstNameError = document.querySelector(".user-input__form-firstname-error");
+const lastNameError = document.querySelector(".user-input__form-lastname-error");
+const emailError = document.querySelector(".user-input__form-email-error");
+const passwordError = document.querySelector(".user-input__form-password-error");
+
+
+
+//-----------------  Functions  -----------------
+
+const hideAllErrorMessages = () => {
+    firstNameError.style.display = "none";
+    lastNameError.style.display = "none";
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
+}
+
+const clearAllInputValues = () => {
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    password.value = "";
+}
+
+//hide all error messages at page load
+window.onload = () => {
+
+    //email.type = "email";
+
+    hideAllErrorMessages();
+    clearAllInputValues();
+}
+
+//get values from each of the fields
+//check if value is null / empty
+//show / hide error message against each of the fields
+const checkInputFields = (e) => {
+
+    //https://wesbos.com/javascript/05-events/prevent-default-and-form-events
+    //Prevent Default and Form Events
+
+    e.preventDefault();
+
+    //console.log(firstName.value + "  " + lastName.value + "   " + email.value + "  " + password.value);
+
+
+    if (firstName.value.trim() === "") {
+        firstNameError.style.display = "block";
+    }
+
+    if (lastName.value.trim() === "") {
+        lastNameError.style.display = "block";
+    }
+
+    //https://stackoverflow.com/a/13975255/2607372
+    //http://jsfiddle.net/boldewyn/2b6d5/
+    email.type = "email";
+    console.log("email " + email.value + "  " + email.checkValidity());
+
+    if (email.checkValidity() === false) {
+        emailError.style.display = "block";
+    }
+
+    if (password.value.trim() === "") {
+        passwordError.style.display = "block";
+    }
+
+    console.log("form is submitted");
+
+}
+
+
+//-----------------  Event Listeners  -----------------
+
+form.addEventListener("submit", checkInputFields);
